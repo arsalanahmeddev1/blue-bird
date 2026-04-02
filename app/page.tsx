26064dc0f78d53b40e6127b69c67c5da48de537c
Arsalan Ahmed <arsalanahmeddev1@gmail.com>
2026-04-02 22:56:56 +0500

    commit before pull

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <h1 className="text-primary bg-secondary border-[100]">home page</h1>
+     <h1 className="text-primary bg-secondary border-[100] border-secondary">home page</h1>
   );
 }
